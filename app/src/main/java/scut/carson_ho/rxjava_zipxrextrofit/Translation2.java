dd7772ba86db5d3bf0150a2a80bdde3f1ecf2f22
Carson.Ho <carson.ho@foxmail.com>
2017-07-21 09:42:59 +0800

    Initial commit

---
+++ app/src/main/java/scut/carson_ho/rxjava_zipxrextrofit/Translation2.java
@@ -0,0 +1,24 @@
+package scut.carson_ho.rxjava_zipxrextrofit;
+
+/**
+ * Created by Carson_Ho on 17/7/21.
+ */
+
+public class Translation2 {
+    private int status;
+    private content content;
+    private static class content {
+        private String from;
+        private String to;
+        private String vendor;
+        private String out;
+        private int errNo;
+    }
+
+    //定义 输出返回数据 的方法
+    public String show() {
+
+        return ("第2次翻译=" + content.out);
+
+    }
+}
